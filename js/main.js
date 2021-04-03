3ee21740ff6fe783b1cc4e28d33ba094c55ac633
Omid Heidarzadeh <arashmidos1370@yahoo.com>
2021-04-03 19:07:06 +0430

    add favicon, fix minor bugs.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
-    collapsibles.forEach(elm => { if (elm != this) elm.classList.toggle("collapsible--expanded", false) });
-    setTimeout(() => { this.classList.toggle("collapsible--expanded") }, 250);
+    this.classList.toggle("collapsible--expanded");
   })
 );
